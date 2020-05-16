e8bf495a85c7b8b9934d84ff79e6d5ff5de1b983
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-05-16 09:58:17 +0200

    Interfaces

--- main.ts
+++ main.ts
@@ -7,9 +7,10 @@ class Hero {
 }
 class Warrior extends Hero {
   private _name: string
-  public arme: string
-  constructor () {
+  public caracteristics: ICaracteristic
+  constructor (caracteristics: ICaracteristic) {
     super ()
+    this.caracteristics = caracteristics
   }
   getName (): string {
     return this._name
@@ -25,30 +26,80 @@ class Warrior extends Hero {
   }
 }
 
-let w: Warrior
-w = new Warrior()
-w.setName('Conan')
-w.setType('Warrior')
-w.arme = 'Epée'
-console.log(w.getName())
-console.log(w.getType())
-console.log(w.arme)
+// Les interfaces
 
-class Warrior2 extends Hero {
-    private _name: string
-    constructor () {
-      super ()
-    }
-    get name (): string {
-      return this._name
-    }
-    set name (name: string) {
-      this._name = name
-    }
+interface ICaracteristic {
+  strength: number,
+  intelligence: number,
+  weapon: string,
+  agility?: number, // pour rendre facultatif
+  speed: number,
+  gender: 'Male' | 'Female', // pour limiter la saisie à une liste
+  onAttack: (damage: number) => void
+}
+
+const carac: ICaracteristic = {strength:10, intelligence:1, weapon:'axe', speed: 7, gender:'Male',onAttack: (damage: number) => console.log('Le personnage a infligé '+damage+' de dégât')}
+
+const w: Warrior = new Warrior(carac)
+w.setName('Conan')
+console.log(w.getName())
+console.log(w.caracteristics)
+w.caracteristics.onAttack(5)
+
+const carac2: ICaracteristic = {strength:4, intelligence:9, weapon:'sword', agility: 5, speed: 8, gender:'Male',onAttack: (damage: number) => console.log('Le personnage a infligé '+damage+' de dégât')}
+
+const w2: Warrior = new Warrior(carac2)
+w2.setName('Kull')
+console.log(w2.getName())
+console.log(w2.caracteristics)
+w2.caracteristics.onAttack(3)
+
+// Héritage entre interface
+
+interface Shape {
+  readonly color: string // propriété uniquement instanciable à la construction comme const
+}
+
+interface Square extends Shape {
+  size: number
+}
+
+const s: Square = {
+  size: 5, color: "red"
+}
+
+console.log (s)
+s.size = 10
+console.log (s)
+//s.color = 'red' pas permis
+
+// Implémentation d'une interface par une classe
+
+class mySquare implements Square {
+  size: number
+  color: string
+  constructor(square: Square) {
+    this.size = square.size
+    this.color = square.color
   }
-  
-  let w2: Warrior2
-  w2 = new Warrior2()
-  w2.name = 'Conan'
-  console.log(w2.name)
-  
+}
+
+// Union d'interfaces
+
+interface Identity {
+  firstName: string,
+  lastName: string
+}
+
+interface Size {
+  height: number,
+  weight: number
+}
+
+const human: Identity & Size = {
+  firstName: 'John',
+  lastName: 'Rambo',
+  height: 180,
+  weight: 80
+}
+
