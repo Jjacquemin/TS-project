ae6341aee3c7e840d0d8cccf64f0cb9da3be879c
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-05-16 08:11:59 +0200

    Enumérations

--- main.ts
+++ main.ts
@@ -1,24 +1,30 @@
-// Déclaration et symbol
-const sym1 = Symbol(0)
-/*
-via la commande tsc -w main.ts
-main.ts(2,12): error TS2585: 'Symbol' only refers to a type, but is being used as a value here. Do you need to change your target library? Try changing the `lib` compiler option to es2015 or later.
-via la commande tsc -w maint.ts --target es6
-ok
-*/
-const sym2 = Symbol('key')
-const sym3 = Symbol(0)
-const getBlabla = Symbol('getBlabla')
-console.log(sym1)
-console.log(sym2)
-console.log(sym3)
-console.log(sym1 === sym3)
-
-const obj = {
-  [sym1]: 'xxxx',
-  [getBlabla] () {
-    return 'blabla'
-  }
+// Les énumérations
+enum Color {
+    red,
+    blue,
+    green
 }
-console.log(obj[sym1])
-console.log(obj[getBlabla]())
+console.log(Color.red);
+console.log(Color.blue);
+console.log(Color.green);
+console.log(Color[0]);
+
+enum Color2 {
+    red = 10,
+    blue,
+    green
+}
+console.log(Color2.red);
+console.log(Color2.blue);
+console.log(Color2.green);
+console.log(Color2[11]);
+
+enum Color3 {
+    red,
+    blue = 10,
+    green
+}
+console.log(Color3.red);
+console.log(Color3.blue);
+console.log(Color3.green);
+console.log(Color3[0]);
