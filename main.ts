282aa860889cb23d1b39132d34e44acf6cdddd0b
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-05-16 07:23:33 +0200

    Déclaration tableaux

--- main.ts
+++ main.ts
@@ -1,10 +1,21 @@
-// Déclaration et types de base
-let a: number;
-let b: string;
-a = 1;
-b = "salut";
-const test: string = "TEST";
-let d: boolean = true;
-d = false;
+// Déclaration et tableaux
+const foods: string[] = ["banane", "fraise"];
+const numbers: number[] = [1, 2];
+const foods2: Array<string> = ["banane", "fraise"];
+
+let tabs: [string, number, boolean];
+tabs = ["a", 1, true];
+tabs[0] = "texte";
+tabs[1] = 5;
+tabs[2] = false;
+
+console.log(tabs);
+
+const a: string = "salut";
+const names: string[] = [`${a} John`, `${a} Kevin`];
+
+console.log(names);
+
+
 
 
